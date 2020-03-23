5c71004ed85bc44c6037544f21bef3c24a68fa5f
Rogue Elephant <jup.trandell@gmail.com>
2020-03-23 17:06:06 +0000

    added loading screen interceptor

---
+++ src/app/shared/services/loading.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingService {
+  constructor() { }
+  public isLoading = new Subject<boolean>();
+  public show = () => this.isLoading.next(true);
+  public hide = () => this.isLoading.next(false);
+}
